667a1b55e07c8fc568163938416f363816cc6ddf
Bangwooju <phonestudio97@gmail.com>
2021-07-08 10:21:43 +0900

    주소참조

--- bangwooju/teststring.java
+++ bangwooju/teststring.java
@@ -8,12 +8,12 @@ public class teststring {
 
     Scanner sc = new Scanner(System.in);
 
-    String name = sc.nextLine();
-    String name2 = sc.nextLine();
+    String name = sc.nextLine(); // 같은 값을 입력해보자
+    String name2 = sc.nextLine();//
 
 
-    System.out.println(a == b);
-    System.out.println(name == name2);
+    System.out.println(a == b); // true결과값이 나온다.
+    System.out.println(name == name2); // false결과값이 나온다. 값이 같아도 Scanner에 저장된 주소가 다르기 때문
 
   };
 
